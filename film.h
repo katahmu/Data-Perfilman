9b282f0354ca318618a912200b1f863b99c41a56
OnionDaGr81 <immanuelwirasatria4@gmail.com>
2025-12-16 21:36:28 +0700

    Define film.h
    Added:
    - Definisi data
    - pointer
    - List child (film)
    -List relasi

--- film.h
+++ film.h
@@ -1,1 +1,45 @@
+#ifndef film_h
+#define film_h
 
+#include <string>
+#include <iostream>
+using namespace std;
+
+// Definisi data
+struct Aktor {
+    string nama;
+    string agensi;
+    int umur;
+};
+
+struct Film {
+    string judul;
+    string genre;
+    double rating;
+};
+
+// Pointer
+typedef struct elmAktor *adrAktor;
+typedef struct elmFilm *adrFilm;
+typedef struct elmRelasi *adrRelasi;
+
+// List child (film)
+struct elmFilm {
+    Film info;
+    adrFilm next;
+};
+
+struct ListFilm {
+    adrFilm first;
+};
+
+// List relasi
+struct elmRelasi {
+    adrFilm nextFilm;
+    adrRelasi next;
+};
+
+struct listRelasi {
+    adrRelasi first;
+};
+         

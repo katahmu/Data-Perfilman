c6955a5d854b5bd08f376760f56c8b0d0d4bfd76
katakatamu <keishahananta@gmail.com>
2025-12-18 01:46:11 +0700

    Update film.cpp and film.h

--- film.cpp
+++ film.cpp
@@ -1,1 +1,153 @@
+#include "film.h"
 
+// Buat List Aktor Baru
+void createListAktor(ListAktor &LA)
+{
+    LA.first = NULL;
+}
+// Buat List Film Baru
+void createListFilm(ListFilm &LF)
+{
+    LF.first = LF.last = NULL;
+}
+// Node Baru
+adrAktor newAktor(Aktor x)
+{
+    adrAktor p = new elmAktor;
+    p->info = x;
+    p->next = NULL;
+    return p;
+}
+
+adrFilm newFilm(Film x)
+{
+    adrFilm p = new elmFilm;
+    p->info = x;
+    p->prev = p->next = NULL;
+    p->firstRel = NULL;
+    return p;
+}
+adrRelasi newRelasi(adrAktor pA)
+{
+    adrRelasi r = new elmRelasi;
+    r->toAktor = pA;
+    r->next = NULL;
+    return r;
+}
+
+// Aktor
+//  Tambah Aktor ke paling awal
+void insertFirstAktor(ListAktor &LA, adrAktor p)
+{
+    p->next = LA.first;
+    LA.first = p;
+}
+// Tambah Aktor ke paling Terakhir
+void insertLastAktor(ListAktor &LA, adrAktor p)
+{
+    if (LA.first == NULL)
+    {
+        LA.first = p;
+    }
+    else
+    {
+        adrAktor q = LA.first;
+        while (q->next != NULL)
+            q = q->next;
+        q->next = p;
+    }
+}
+// Hapus Aktor yang di paling awal
+void deleteFirstAktor(ListAktor &LA, adrAktor &p)
+{
+    p = LA.first;
+    if (p == NULL)
+        return;
+    LA.first = p->next;
+    p->next = NULL;
+}
+// Hapus Aktir yang di paling Akhir
+void deleteLastAktor(ListAktor &LA, adrAktor &p)
+{
+    p = NULL;
+    if (LA.first == NULL)
+        return;
+
+    if (LA.first->next == NULL)
+    {
+        p = LA.first;
+        LA.first = NULL;
+        return;
+    }
+
+    adrAktor prev = NULL;
+    adrAktor cur = LA.first;
+    while (cur->next != NULL)
+    {
+        prev = cur;
+        cur = cur->next;
+    }
+    prev->next = NULL;
+    p = cur;
+}
+// Hapus Aktor berdasrkan Id
+
+// Cari Aktor berdasarkan Id
+adrAktor findAktorById(ListAktor LA, int idAktor)
+{
+    adrAktor p = LA.first;
+    while (p != NULL && p->info.id != idAktor)
+        p = p->next;
+    return p;
+}
+// Tampilkan semua aktor
+void showAllAktor(ListAktor LA)
+{
+    adrAktor p = LA.first;
+    while (p != NULL)
+    {
+        cout << "ID:" << p->info.id
+             << " | " << p->info.nama
+             << " | " << p->info.agensi
+             << " | umur:" << p->info.umur
+             << " | " << (p->info.gender == 'F' ? "Aktris" : "Aktor")
+             << "\n";
+        p = p->next;
+    }
+}
+//
+// Film
+// Tambah Film di paling awal
+void insertFirstFilm(ListFilm &LF, adrFilm p) {};
+// Tambah Film di paling Akhir
+void insertLastFilm(ListFilm &LF, adrFilm p) {};
+// Hapus Film Pertama
+void deleteFirstFilm(ListFilm &LF, adrFilm &p) {};
+// Hapus Film Terakhir
+void deleteLastFilm(ListFilm &LF, adrFilm &p) {};
+// Hapus Film berdasarkan Id
+void deleteFilmById(ListFilm &LF, int idFilm) {};
+// Cari Film berdasarkan Id
+adrFilm findFilmById(ListFilm LF, int idFilm) {};
+// Tampilkan Semua Film
+void showAllFilm(ListFilm LF);
+
+// Relasi
+// Menghubungkan Film dengan Aktor
+void connectFilmAktor(ListFilm &LF, ListAktor &LA, int idFilm, int idAktor) {};
+// Hapus Aktor dari sebuah Film
+bool deleteAktorFromFilm(ListFilm &LF, int idFilm, int idAktor) {};
+// Hitung Aktor yang ada di dalam Film
+int countAktorInFilm(ListFilm LF, int idFilm) {};
+
+// Tampilkan
+// Tampilkan Film beserta Aktor
+void showFilmWithAktor(ListFilm LF) {};
+// Tampilkan Film Berdasarkan Aktor
+void showFilmByAktor(ListFilm LF, ListAktor LA, int idAktor) {};
+// Tampilkan Aktor Berdasarkan Film
+void showAktorByFilm(ListFilm LF, int idFilm) {};
+
+// Top
+// Tampilkan Aktor paling ketceh
+void showTopAktorDanAktris(ListFilm LF, ListAktor LA) {};
--- film.h
+++ film.h
@@ -1,18 +1,23 @@
-#ifndef film_h
-#define film_h
+#ifndef FILM_H
+#define FILM_H
 
-#include <string>
 #include <iostream>
+#include <string>
 using namespace std;
 
 // Definisi data
-struct Aktor {
+struct Aktor
+{
+    int id;
     string nama;
     string agensi;
     int umur;
+    char gender;
 };
 
-struct Film {
+struct Film
+{
+    int id;
     string judul;
     string genre;
     double rating;
@@ -23,23 +28,76 @@ typedef struct elmAktor *adrAktor;
 typedef struct elmFilm *adrFilm;
 typedef struct elmRelasi *adrRelasi;
 
-// List child (film)
-struct elmFilm {
-    Film info;
-    adrFilm next;
+// List Aktor
+struct elmAktor
+{
+    Aktor info;
+    adrAktor next;
 };
 
-struct ListFilm {
+struct ListAktor
+{
+    adrAktor first;
+};
+
+// List child (film)
+
+struct elmFilm
+{
+    Film info;
+    adrFilm prev;
+    adrFilm next;
+    adrRelasi firstRel;
+};
+
+struct ListFilm
+{
     adrFilm first;
+    adrFilm last;
 };
 
 // List relasi
-struct elmRelasi {
-    adrFilm nextFilm;
+struct elmRelasi
+{
+    adrAktor toAktor;
     adrRelasi next;
 };
 
-struct listRelasi {
-    adrRelasi first;
-};
-         
+void createListAktor(ListAktor &LA); // Buat List Aktor Baru
+void createListFilm(ListFilm &LF);   // Buat List Film Baru
+
+// Tambah Aktor/Film/Relasi Baru
+adrAktor newAktor(Aktor x);
+adrFilm newFilm(Film x);
+adrRelasi newRelasi(adrAktor pA);
+
+// Aktor (SLL)
+void insertFirstAktor(ListAktor &LA, adrAktor p);               // Tambah Aktor di paling awal
+void insertLastAktor(ListAktor &LA, adrAktor p);                // Tambah Aktor di paling akhir
+void deleteFirstAktor(ListAktor &LA, adrAktor &p);              // Hapus Aktor Pertama
+void deleteLastAktor(ListAktor &LA, adrAktor &p);               // Hapus Aktor Terakhir
+void deleteAktorById(ListAktor &LA, ListFilm &LF, int idAktor); // Hapus Aktor Berdasarkan Id
+adrAktor findAktorById(ListAktor LA, int idAktor);              // Cari Aktor berdasarkan Id
+void showAllAktor(ListAktor LA);                                // Tampilkan Semua Aktor
+
+// Film (DLL)
+void insertFirstFilm(ListFilm &LF, adrFilm p);  // Tambah Film di paling awal
+void insertLastFilm(ListFilm &LF, adrFilm p);   // Tambah Film di paling Akhir
+void deleteFirstFilm(ListFilm &LF, adrFilm &p); // Hapus Film Pertama
+void deleteLastFilm(ListFilm &LF, adrFilm &p);  // Hapus Film Terakhir
+void deleteFilmById(ListFilm &LF, int idFilm);  // Hapus Film berdasarkan Id
+adrFilm findFilmById(ListFilm LF, int idFilm);  // Cari Film berdasarkan Id
+void showAllFilm(ListFilm LF);                  // Tampilkan Semua Film
+
+// Relasi
+
+
+// Tampilkan
+void showFilmWithAktor(ListFilm LF);                          // Tampilkan Film beserta Aktor
+void showFilmByAktor(ListFilm LF, ListAktor LA, int idAktor); // Tampilkan Film Berdasarkan Aktor
+void showAktorByFilm(ListFilm LF, int idFilm);                // Tampilkan Aktor Berdasarkan Film
+
+// Top
+void showTopAktorDanAktris(ListFilm LF, ListAktor LA); // Tampilkan Aktor paling ketceh
+
+#endif

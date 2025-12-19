763f6fa5efec6056e1bfe5c26c0f7a20833d8818
OnionDaGr81 <immanuelwirasatria4@gmail.com>
2025-12-20 03:34:29 +0700

    updated film.h
    added beberapa function sesuai keinginan fungsionalitas baru

--- film.h
+++ film.h
@@ -72,8 +72,8 @@ adrFilm newFilm(Film x);
 adrRelasi newRelasi(adrAktor pA);
 
 // Aktor (SLL)
-void insertFirstAktor(ListAktor &LA, adrAktor p);               // Tambah Aktor di paling awal
-void insertLastAktor(ListAktor &LA, adrAktor p);                // Tambah Aktor di paling akhir
+void insertFirstAktor(ListAktor &LA, adrAktor p);               // Poin 1 & 6. Tambah Aktor di paling awal
+void insertLastAktor(ListAktor &LA, adrAktor p);                // Poin 1 & 6. Tambah Aktor di paling akhir
 void deleteFirstAktor(ListAktor &LA, adrAktor &p);              // Hapus Aktor Pertama
 void deleteLastAktor(ListAktor &LA, adrAktor &p);               // Hapus Aktor Terakhir
 void deleteAktorById(ListAktor &LA, ListFilm &LF, int idAktor); // Hapus Aktor Berdasarkan Id
@@ -81,21 +81,47 @@ adrAktor findAktorById(ListAktor LA, int idAktor);              // Cari Aktor be
 void showAllAktor(ListAktor LA);                                // Tampilkan Semua Aktor
 
 // Film (DLL)
-void insertFirstFilm(ListFilm &LF, adrFilm p);  // Tambah Film di paling awal
-void insertLastFilm(ListFilm &LF, adrFilm p);   // Tambah Film di paling Akhir
+void insertFirstFilm(ListFilm &LF, adrFilm p);  // Poin 1 & 6.Tambah Film di paling awal
+void insertLastFilm(ListFilm &LF, adrFilm p);   // Poin 1 & 6.Tambah Film di paling Akhir
 void deleteFirstFilm(ListFilm &LF, adrFilm &p); // Hapus Film Pertama
 void deleteLastFilm(ListFilm &LF, adrFilm &p);  // Hapus Film Terakhir
 void deleteFilmById(ListFilm &LF, int idFilm);  // Hapus Film berdasarkan Id
 adrFilm findFilmById(ListFilm LF, int idFilm);  // Cari Film berdasarkan Id
 void showAllFilm(ListFilm LF);                  // Tampilkan Semua Film
 
-// Relasi
+// Poin 3. Menghapus data parent beserta relasinya
+void deleteAktorById(ListAktor &LA, ListFilm &LF, int idAktor);
+void deleteFilmById(ListFilm &LF, int idFilm);
 
+// Poin 4. Mencari data parent
+adrAktor findAktorById(ListAktor LA, int idAktor);
+
+// Poin 5. Mencari data child
+adrFilm findFilmById(ListFilm LF, int idFilm);
+
+// Relasi
+// Poin 7. Menghubungkan Film dengan Aktor
+void connectFilmAktor(ListFilm &LF, ListAktor &LA, int idFilm, int idAktor);
+// Poin 7. Menghubungkan Aktor dengan Film
+void connectAktorFilm(ListAktor &LA, ListFilm &LF, int idAktor, int idFilm);
+
+// Poin 9. Mencari data child pada parent tertentu
+adrFilm findFilmByAktor(ListFilm LF, ListAktor LA, int idAktor);
+
+// Poin 10. Menghapus data child pada parent tertentu beserta relasinya
+void deleteFilmByAktor(ListFilm &LF, ListAktor &LA, int idAktor, int idFilm);
+
+// Poin 11. Menghitung jumlah data child dari parent tertentu
+int countFilmByAktor(ListFilm LF, ListAktor LA, int idAktor);
+
+// Poin 12. Main program
+int main();
 
 // Tampilkan
-void showFilmWithAktor(ListFilm LF);                          // Tampilkan Film beserta Aktor
-void showFilmByAktor(ListFilm LF, ListAktor LA, int idAktor); // Tampilkan Film Berdasarkan Aktor
-void showAktorByFilm(ListFilm LF, int idFilm);                // Tampilkan Aktor Berdasarkan Film
+void showFilmWithAktor(ListFilm LF);                          // Poin 8. Tampilkan Film beserta Aktor
+void showFilmByAktor(ListFilm LF, ListAktor LA, int idAktor); // Poin 8. Tampilkan Film Berdasarkan Aktor
+void showAktorByFilm(ListFilm LF, int idFilm);                // Poin 8. Tampilkan Aktor Berdasarkan Film
+void showAktorWithFilm(ListAktor LA, ListFilm LF);          //   Poin 8. Tampilkan aktor beserta Film
 
 // Top
 void showTopAktorDanAktris(ListFilm LF, ListAktor LA); // Tampilkan Aktor paling ketceh
